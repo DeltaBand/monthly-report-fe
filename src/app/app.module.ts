defa992f34f52fcad52a7af3d2ee6cb5689eacbe
Marranica <marranica@git.com>
2022-04-09 00:57:01 +0100

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthenticationModule],
+  imports: [BrowserModule, AppRoutingModule, AuthenticationModule, BrowserAnimationsModule],
   providers: [{ provide: APP_BASE_HREF, useValue: '/mor' }],
   bootstrap: [AppComponent],
 })
